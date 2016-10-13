0e4673a9dd5a9c11035d181439821f67bf333713
bernimoses <bernimoses@dnsproject.de>
2016-10-13 16:32:01 +0200

    Initial commit.

---
+++ Table.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Table here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Table extends Actor
+{
+    /**
+     * Act - do whatever the Table wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
