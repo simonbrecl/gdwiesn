5c57c011a387c71bf6c1d304cf157b3a3f7c0c0f
Simon Brecl <simon.brecl@gmail.com>
2017-01-12 15:11:27 +0100

    lives starts again after new level

--- UpgradeScreen.java
+++ UpgradeScreen.java
@@ -3,6 +3,7 @@ import greenfoot.Greenfoot;
 import greenfoot.MouseInfo;
 import greenfoot.World;
 
+
 /**
  * Created by ericasolum on 12/1/16.
  */
@@ -12,7 +13,8 @@ public class UpgradeScreen extends World {
     private int money;
     public TentState tentState;
     public Upgrademap upgrademap;
-    
+    private LevelBase world;
+
     public UpgradeScreen(int money, TentState state) {
         super(800, 600, 1);
         this.money = money;
@@ -23,6 +25,7 @@ public class UpgradeScreen extends World {
 
     public void prepare() {
         upgrademap = new Upgrademap("levels/Upgrade-Screen.xml", this, tentState);
+        world = new LevelBase(2, new TentState(), "levels/MyWorld.xml");
 
     }
 
@@ -37,17 +40,17 @@ public class UpgradeScreen extends World {
 
     public void goToNextDay() {
         tentState.increaseDay();
-        if(tentState.getDay() == 2) {
+        if (tentState.getDay() == 2) {
             Level2 level = new Level2(tentState);
             Greenfoot.setWorld(level);
-        }
-        else if(tentState.getDay() == 3) {
+            world.getHeart2().getImage().setTransparency(255);
+            world.getHeart3().getImage().setTransparency(255);
+            Customer.counter1=0;
+        } else if (tentState.getDay() == 3) {
             //Says level 2 for now but change this later
             Level2 level = new Level2(tentState);
             Greenfoot.setWorld(level);
         }
 
     }
-
-
 }
