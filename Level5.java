68163d71ba5d9902539a2c1fa3532f44acf90cf9
Simon Brecl <simon.brecl@gmail.com>
2017-01-18 13:30:49 +0100

    2 days per level, difficulty fix

---
+++ Level5.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Level2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Level5 extends LevelBase {
+
+    /**
+     * Constructor for object of class Level2.
+     */
+
+    public Level5(TentState state) {
+        super(9, 500, state, "levels/MyWorld.xml");
+
+        setMinPerLevel(1);
+        setMaxPeople(20);
+        setMinPeople(5);
+        setInterval(5);
+    }
+
+    public void act() {
+        super.act();
+    }
+}
---
+++ Level8.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Level2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Level8 extends LevelBase {
+
+    /**
+     * Constructor for objects of class Level2.
+     */
+
+    public Level8(TentState state) {
+        super(15, 800, state, "levels/MyWorld.xml");
+
+        setMinPerLevel(2);
+        setMaxPeople(20);
+        setMinPeople(5);
+        setInterval(4);
+    }
+
+    public void act() {
+        super.act();
+    }
+}
